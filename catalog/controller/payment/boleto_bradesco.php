377ee77989e9ef0de1fc68a3b98ed555c955edb9
Valdeir Santana <valdeirpsr@hotmail.com.br>
2012-08-02 00:40:59 -0300

    Correção do valor total

--- catalog/controller/payment/boleto_bradesco.php
+++ catalog/controller/payment/boleto_bradesco.php
@@ -124,7 +124,7 @@ $dadosboleto["instrucoes4"] = $this->config->get('boleto_bradesco_instrucoes4');
 // DADOS DO BOLETO PARA O SEU CLIENTE
 $dias_de_prazo_para_pagamento = $this->config->get('boleto_bradesco_dia_prazo_pg');
 $data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
-$valor_cobrado = $order_info['total']; // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+$valor_cobrado = number_format($order_info['total']*$order_info['currency_value'], 2, ',', '.'); // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
 $valor_cobrado = str_replace(",", ".",$valor_cobrado);
 $valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
 
